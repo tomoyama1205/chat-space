22385de8395210aa41a57939eeb1435234668c51
tomoyama1205 <tomoyama1205@gmail.com>
2019-06-30 16:19:40 +0900

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chat-space1205'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1205'
+  config.fog_directory  = 'tomoyama1205'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tomoyama1205'
 end
