501193e57077285782db40e1f314e544c6036c57
tomoyama1205 <tomoyama1205@gmail.com>
2019-07-01 17:41:09 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resource :user, only: [:edit, :update]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
     namespace :api do
